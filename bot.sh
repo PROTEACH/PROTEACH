0582b35fe29f313207e56153fa92367fb2fba2af
DARK-H4CK3R <darkshacker623@gmail.com>
2024-12-04 10:36:53 +0530

    Create bot.sh

---
+++ bot.sh
@@ -0,0 +1,249 @@
+#!/bin/bash
+
+clear
+# dx color
+r='\033[1;91m'
+p='\033[1;95m'
+y='\033[1;93m'
+g='\033[1;92m'
+n='\033[1;0m'
+b='\033[1;94m'
+c='\033[1;96m'
+
+# dx Symbol
+X='\033[1;92m[\033[1;00m⎯꯭̽𓆩\033[1;92m]\033[1;96m'
+D='\033[1;92m[\033[1;00m〄\033[1;92m]\033[1;93m'
+E='\033[1;92m[\033[1;00m×\033[1;92m]\033[1;91m'
+A='\033[1;92m[\033[1;00m+\033[1;92m]\033[1;92m'
+C='\033[1;92m[\033[1;00m</>\033[1;92m]\033[92m'
+lm='\033[96m▱▱▱▱▱▱▱▱▱▱▱▱\033[0m〄\033[96m▱▱▱▱▱▱▱▱▱▱▱▱\033[1;00m'
+dm='\033[93m▱▱▱▱▱▱▱▱▱▱▱▱\033[0m〄\033[93m▱▱▱▱▱▱▱▱▱▱▱▱\033[1;00m'
+
+dx() {
+clear
+echo
+echo -e "		       ${g}Hey \033[1;92mI'm\033[1;92m \033[1;92mDx-\033[1;92mSimu"
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^ω^${c})"
+echo -e "             ⊂(___)づ     ⋅˚₊‧ ଳ ‧₊˚ ⋅"
+echo ""
+sleep 3
+}
+normal() {
+clear
+echo
+echo -e "		       ${g}Uhu"
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo ""
+sleep 1
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${n}▪︎_▪︎${c})"
+echo -e "             ⊂(___)づ"
+echo ""
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1
+}
+10() {
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${n}•_•${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${n}▪︎_▪︎${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}>_>${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}<_<${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${n}▪︎_▪︎${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1
+clear
+}
+
+1() {
+clear
+echo
+echo -e "	                .  ${g}ᶻ 𝗓 𐰁 .ᐟ..."
+echo -e "${c}              (\_/) ."
+echo -e "              (${y}-_-${c})."
+echo -e "             ⊂(___)づ"
+echo
+sleep 5
+clear
+echo
+echo -e "		       ${g}Uhu"
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo ""
+sleep 1
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${n}▪︎_▪︎${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 0.5
+clear
+echo
+echo
+echo -e "${c}              (\_/)"
+echo -e "              (${y}^_^${c})"
+echo -e "             ⊂(___)づ"
+echo ""
+sleep 1
+}
+2() {
+clear
+echo
+echo -e "	            	${g}Wow.."
+echo -e "${c}              (\_/)"
+echo -e "              (${y}°□°${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1.5
+}
+3() {
+clear
+echo
+echo -e "		         ${h}Angry..."
+echo -e "${c}              (\_/)"
+echo -e "              (${r}⳻_⳺${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 2
+}
+4() {
+clear
+echo
+echo -e "	                 ${g}Headache...*"
+echo -e "${c}              (\_/)"
+echo -e "              (${n}*—*${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1.5
+}
+5() {
+clear
+echo
+echo -e "		          ${g}Cute...♡"
+echo -e "${c}              (\_/)"
+echo -e "              (${p}♡ω♡${c})"
+echo -e "             ⊂(___)づ ᯓᡣ𐭩"
+echo
+sleep 2
+}
+6() {
+clear
+echo
+echo -e "       	           ${g} What?"
+echo -e "              ${c}(\_/)"
+echo -e "              (${n}¤_¤${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 1.5
+}
+7() {
+clear
+echo
+echo -e "		          ${g}Meowww..."
+echo -e "${c}              (\_/)"
+echo -e "              (${g}>ω>${c})"
+echo -e "             ⊂(___)づ"
+echo
+sleep 2
+}
+functions=("normal" "10" "1" "2" "10" "3" "4" "7" "5" "normal" "6" "10" "10" "normal")
+dx
+while true; do
+    # Choose a random function from the array
+    rand_func=$((RANDOM % ${#functions[@]}))
+    chosen_function=${functions[$rand_func]}
+
+    # Call the chosen function
+    $chosen_function
+
+    # Sleep for 5 seconds
+    sleep 0
+done
+normal
